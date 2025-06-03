874061e582290eddd1fc288658de78d1f96b3a93
liyheng <86578139+liyheng@users.noreply.github.com>
2025-06-03 11:23:54 +0800

    Add files via upload

---
+++ scripts/train_humanoidstandup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#env=NoisyStatelessHumanoidStandupHard
+#env=NoisyStatelessHumanoidStandupMedium
+#env=NoisyStatelessHumanoidStandupEasy
+env=StatelessHumanoidStandup
+seed_max=2
+for seed in `seq ${seed_max}`; do
+    echo "seed is ${seed}:"
+    CUDA_VISIBLE_DEVICES=0 python GPO.py --env_name ${env} --seed ${seed} \
+    --unroll_length 5 --batch_size 256 --num_minibatches 32 --num_update_epochs 8\
+    --reward_scaling 0.1 --entropy_cost 0.01 --discounting 0.99 --target_kl 0.001 \
+    --eps 0.3 --alpha 3 --use_clip True
+done
---
+++ scripts/train_inverteddoublependulum.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#env=NoisyStatelessInvertedDoublePendulumHard
+#env=NoisyStatelessInvertedDoublePendulumEasy
+#env=NoisyStatelessInvertedDoublePendulumMedium
+env=StatelessInvertedDoublePendulum
+seed_max=20
+for seed in `seq ${seed_max}`; do
+    echo "seed is ${seed}:"
+    CUDA_VISIBLE_DEVICES=0 python GPO.py --env_name ${env} --seed ${seed} \
+    --unroll_length 20 --batch_size 256 --num_minibatches 8 --num_update_epochs 4 \
+    --reward_scaling 1 --entropy_cost 0.01 --discounting 0.997  --target_kl 0.001 \
+    --eps 0.1 --alpha 0 --use_clip True
+done
+
